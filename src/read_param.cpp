26320917dbf59aa3e274783ab651abbdbc2c0dd3
Bence Magyar <bence.magyar.robotics@gmail.com>
2018-02-06 14:10:12 +0000

    Add parsing list

--- src/read_param.cpp
+++ src/read_param.cpp
@@ -10,5 +10,15 @@ int main(int argc, char** argv)
 
   ROS_INFO_STREAM("Int param: " << number);
 
+  std::vector<double> numbers;
+  n.param("list", numbers, std::vector<double>());
+
+  ROS_INFO_STREAM("3nd number in list: " << numbers[2]);
+
+  std::vector<int> ints;
+  n.param("list", ints, std::vector<int>());
+
+  ROS_INFO_STREAM("3nd number in list: " << ints[2]);
+
   return 0;
 }
